7b06507511b12e06f2a235f1f4759bed63c3edb3
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2022-12-02 22:12:20 -0500

    Initial commit with some scripts ans assets

---
+++ Assets/Player/Scripts/Mover.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+        void Update()
+        {
+            UpdateAnimator();
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            GetComponent<NavMeshAgent>().destination = destination;
+        }
+
+        void UpdateAnimator()
+        {
+            Vector3 playerVelocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(playerVelocity);
+            float speed = localVelocity.z;
+
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+    }
+}
---
+++ Assets/Player/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Movement;
+
+namespace RPG.Control
+{
+    public class PlayerController : MonoBehaviour
+    {
+        void Update() {
+            if (Input.GetMouseButton(0))
+            {
+                MoveToCursor();
+            }
+         }
+
+       void MoveToCursor()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(ray, out hit);
+            if (hasHit)
+            {
+                GetComponent<Mover>().MoveTo(hit.point);
+            }
+        }
+    }
+}
