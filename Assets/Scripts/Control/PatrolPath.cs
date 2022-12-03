e4aace7a5ff303456efbe04826dbd0c77fe69b65
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2022-12-03 08:32:07 -0500

    Add tool for visualizing waypoint
    Created a tool that is now a prefab that uses the children of the
    parent object to draw sphere at that point and draw lines between
    the points to show the path.

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        const float waypointGizmoRadius = .1f;
+
+        void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                int j = GetNextIndex(i);
+                Gizmos.DrawSphere(GetWaypoint(i), waypointGizmoRadius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
+            }
+        }
+
+        int GetNextIndex(int i)
+        {
+            if (i + 1 == transform.childCount)
+            {
+                return 0;
+            }
+                return i + 1;
+            
+        }
+
+        Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
+        }
+    }
+}
