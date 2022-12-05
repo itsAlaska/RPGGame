2aff757e61670b4dd1dc0838075e0a5a82aa1f5f
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2022-12-05 08:20:41 -0500

    Add cinematic and scripts to control player
    Or more, take the control of the player away from the user so that
    they can't move during the cutscene.

---
+++ Assets/Scripts/Cinematic/ControlRemover.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using RPG.Core;
+using RPG.Control;
+
+namespace RPG.Cinematics
+{
+    public class ControlRemover : MonoBehaviour
+    {
+        GameObject player;
+
+        void Start()
+        {
+            player = GameObject.FindWithTag("Player");
+            GetComponent<PlayableDirector>().played += DisableControl;
+            GetComponent<PlayableDirector>().stopped += EnableControl;
+        }
+
+        void OnFinish() { }
+
+        void DisableControl(PlayableDirector pd)
+        {
+            print(player.name);
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<PlayerController>().enabled = false;
+        }
+
+        void EnableControl(PlayableDirector pd)
+        {
+            player.GetComponent<PlayerController>().enabled = true;
+        }
+    }
+}
