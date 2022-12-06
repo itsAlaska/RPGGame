7ad5a4bd1891b68d621013e40182324a5f2fd786
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2022-12-06 02:06:09 -0500

    Implement saving / loading system
    Currently only is saving the position of the player and the enemies.

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using RPG.Core;
 using UnityEngine;
 using UnityEngine.AI;
+using RPG.Saving;
 
 namespace RPG.Movement
 {
-    public class Mover : MonoBehaviour, IAction
+    public class Mover : MonoBehaviour, IAction, ISaveable
     {
-        [SerializeField] float maxSpeed = 6;
+        [SerializeField]
+        float maxSpeed = 6;
 
         NavMeshAgent navMeshAgent;
         Health health;
-    
+
         bool isAttacking;
 
         void Start()
@@ -53,6 +55,19 @@ namespace RPG.Movement
             float speed = localVelocity.z;
 
             GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+
+        public object CaptureState()
+        {
+            return new SerializableVector3(transform.position);
+        }
+
+        public void RestoreState(object state)
+        {
+            SerializableVector3 position = (SerializableVector3)state;
+            GetComponent<NavMeshAgent>().enabled = false;
+            transform.position = position.ToVector();
+            GetComponent<NavMeshAgent>().enabled = true;
         }
     }
 }
