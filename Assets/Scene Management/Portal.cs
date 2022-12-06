bf4325346c1d108ab028f93dbda79081f34e5cc3
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2022-12-06 00:12:07 -0500

    Add spawn points to portals

--- Assets/Scene Management/Portal.cs
+++ Assets/Scene Management/Portal.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 namespace RPG.SceneManagement
@@ -9,6 +11,9 @@ namespace RPG.SceneManagement
     {
         [SerializeField]
         int sceneToLoad = -1;
+
+        [SerializeField]
+        Transform spawnPoint;
 
         void OnTriggerEnter(Collider other)
         {
@@ -22,8 +27,29 @@ namespace RPG.SceneManagement
         {
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
-            print("scene loaded");
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
             Destroy(gameObject);
+        }
+
+        void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this)
+                    continue;
+
+                return portal;
+            }
+            return null;
         }
     }
 }
