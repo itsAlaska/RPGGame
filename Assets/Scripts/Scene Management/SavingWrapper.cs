174d49214dfcd4b8e6e334f92e94b081832b153c
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2022-12-06 02:48:50 -0500

    Add load in start, and fade in at start

--- Assets/Scripts/Scene Management/SavingWrapper.cs
+++ Assets/Scripts/Scene Management/SavingWrapper.cs
@@ -9,6 +9,16 @@ namespace RPG.SceneManagement
     public class SavingWrapper : MonoBehaviour
     {
         const string defaultSaveFile = "save";
+        [SerializeField]
+        float fadeInTime = 1f;
+
+        IEnumerator Start() {
+            Fader fader = FindObjectOfType<Fader>();
+            fader.FadeOutImmediate();
+            yield return GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile);
+            yield return fader.FadeIn(fadeInTime);
+
+        }
 
         void Update()
         {
