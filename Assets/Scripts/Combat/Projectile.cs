992d285faf183e8c88f17efd2414fda646947f2f
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2022-12-07 17:21:51 -0500

    Add arrow projectile prefab and script

---
+++ Assets/Scripts/Combat/Projectile.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Projectile : MonoBehaviour
+    {
+        [SerializeField]
+        Transform target = null;
+
+        [SerializeField]
+        float speed = 1;
+
+        void Update()
+        {
+            if (target == null)
+                return;
+
+            transform.LookAt(GetAimLocation());
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+        Vector3 GetAimLocation()
+        {
+            CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
+            
+            if (targetCapsule == null)
+                return target.position;
+
+            return target.position + Vector3.up * targetCapsule.height / 2;
+        }
+    }
+}
