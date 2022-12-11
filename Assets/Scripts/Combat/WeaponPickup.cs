fdd36a4732f6d0356e9e4119cd9732f4299b1b0b
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2022-12-11 00:52:33 -0500

    Add respawn timer to wepaon pickups

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using RPG.Combat;
@@ -10,12 +11,31 @@ namespace RPG.Combat
         [SerializeField]
         Weapon weapon = null;
 
+        [SerializeField]
+        float respawnTime = 5f;
+
         void OnTriggerEnter(Collider other)
         {
             if (other.tag == "Player")
             {
                 other.GetComponent<Fighter>().EquipWeapon(weapon);
-                Destroy(gameObject);
+                StartCoroutine(HideForSeconds(respawnTime));
+            }
+        }
+
+        IEnumerator HideForSeconds(float seconds)
+        {
+            ShowPickup(false);
+            yield return new WaitForSeconds(seconds);
+            ShowPickup(true);
+        }
+
+        void ShowPickup(bool shouldShow)
+        {
+            GetComponent<Collider>().enabled = shouldShow;
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(shouldShow);
             }
         }
     }
