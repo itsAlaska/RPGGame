b33cd0a044980d9531187d912443ae26281287e9
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2022-12-11 01:21:58 -0500

    Trying to add auto formatter

---
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Stats
+{
+    public class BaseStats : MonoBehaviour
+    {
+        [SerializeField]
+        int level = 1;
+
+        
+    }
+}
