b1819e3862528dc45ffce5281bbb6e919eaa61ba
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2022-12-11 01:36:19 -0500

    Add class distinction for base Stats

--- Assets/Scripts/Stats/BaseStats.cs
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -6,9 +6,11 @@ namespace RPG.Stats
 {
     public class BaseStats : MonoBehaviour
     {
+        [Range(1, 99)]
         [SerializeField]
         int level = 1;
 
-        
+        [SerializeField]
+        CharacterClass characterClass;
     }
 }
