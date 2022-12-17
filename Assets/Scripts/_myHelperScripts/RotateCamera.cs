929fefcb28ab9b2d45f605a49cdb67384ecd06b9
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2022-12-16 20:21:47 -0500

    Add right click to move camera script

--- Assets/Scripts/_myHelperScripts/RotateCamera.cs
+++ Assets/Scripts/_myHelperScripts/RotateCamera.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class RotateCamera : MonoBehaviour
 {
     [SerializeField]
-    GameObject mainCamera;
+    float speed = 5f;
 
     void Update() {
         if (Input.GetMouseButton(1))
         {
-            
+            transform.eulerAngles += speed * new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
         }
      }
 }
