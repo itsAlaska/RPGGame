4d7d163006b6de2815b20a90165b4b89f0953b65
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2022-12-16 22:43:29 -0500

    Edit HealthBar to only display when not full/empty

--- Assets/Scripts/Attributes/HealthBar.cs
+++ Assets/Scripts/Attributes/HealthBar.cs
@@ -12,8 +12,21 @@ namespace RPG.Attributes
         [SerializeField]
         RectTransform foreground = null;
 
+        [SerializeField]
+        Canvas rootCanvas = null;
+
         void Update()
         {
+            if (
+                Mathf.Approximately(healthComponent.GetFraction(), 0)
+                || Mathf.Approximately(healthComponent.GetFraction(), 1)
+            )
+            {
+                rootCanvas.enabled = false;
+                return;
+            }
+            
+            rootCanvas.enabled = true;
             foreground.localScale = new Vector3(healthComponent.GetFraction(), 1, 1);
         }
     }
