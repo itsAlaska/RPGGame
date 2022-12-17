1bc92c8bd941fa3f8b05b57332945846b5d85511
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2022-12-16 23:51:11 -0500

    Correct a mistake I made with the fade script

--- Assets/Scripts/Scene Management/Fader.cs
+++ Assets/Scripts/Scene Management/Fader.cs
@@ -43,9 +43,9 @@ namespace RPG.SceneManagement
 
         private IEnumerator FadeRoutine(float target, float time)
         {
-            while (Mathf.Approximately(canvasGroup.alpha, target))
+            while (!Mathf.Approximately(canvasGroup.alpha, target))
             {
-                canvasGroup.alpha += Mathf.MoveTowards(
+                canvasGroup.alpha = Mathf.MoveTowards(
                     canvasGroup.alpha,
                     target,
                     Time.deltaTime / time
