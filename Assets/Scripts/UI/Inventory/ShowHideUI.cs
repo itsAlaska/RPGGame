396f7e19cf74aabd283e5b75060acf35536a7bab
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2022-12-21 19:05:25 -0500

    Begin working on Inventory UI

---
+++ Assets/Scripts/UI/Inventory/ShowHideUI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.UI
+{
+    public class ShowHideUI : MonoBehaviour
+    {
+        [SerializeField]
+        KeyCode toggleKey = KeyCode.Escape;
+
+        [SerializeField]
+        GameObject uiContainer = null;
+
+        void Start()
+        {
+            uiContainer.SetActive(false);
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(toggleKey))
+            {
+                uiContainer.SetActive(!uiContainer.activeSelf);
+            }
+        }
+    }
+}
