bc89d1f705aeb6e244d54edfb980e652e17df58c
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2022-12-29 16:40:03 -0500

    Add ability to save changes to Dialogue Editor

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 namespace RPG.Dialogue
 {
     [CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue", order = 0)]
-    public class Dialogue : ScriptableObject
+    public class Dialogue : ScriptableObject, ISerializationCallbackReceiver
     {
         [SerializeField] private List<DialogueNode> nodes = new List<DialogueNode>();
 
@@ -16,10 +16,7 @@ namespace RPG.Dialogue
 #if UNITY_EDITOR
         private void Awake()
         {
-            if (nodes.Count == 0)
-            {
-                CreateNode(null);
-            }
+            
 
             OnValidate();
         }
@@ -65,6 +62,7 @@ namespace RPG.Dialogue
                 parentNode.children.Add(newNode.name);
             }
             nodes.Add(newNode);
+            
             OnValidate();
         }
 
@@ -82,6 +80,29 @@ namespace RPG.Dialogue
             {
                 node.children.Remove(nodeToDelete.name);
             }
+        }
+
+        public void OnBeforeSerialize()
+        {
+            if (nodes.Count == 0)
+            {
+                CreateNode(null);
+            }
+            
+            if (AssetDatabase.GetAssetPath(this) != "")
+            {
+                foreach (DialogueNode node in GetAllNodes())
+                {
+                    if (AssetDatabase.GetAssetPath(node) == "")
+                    {
+                        AssetDatabase.AddObjectToAsset(node, this);
+                    }
+                }
+            }
+        }
+
+        public void OnAfterDeserialize()
+        {
         }
     }
 }
