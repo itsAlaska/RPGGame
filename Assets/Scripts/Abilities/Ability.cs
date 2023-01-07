9f93a74c0ff5575906ee3e19de97214f46289a20
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2023-01-07 00:48:42 -0500

    Add filters for AoE targets

--- Assets/Scripts/Abilities/Ability.cs
+++ Assets/Scripts/Abilities/Ability.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using RPG.Inventories;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace RPG.Abilities
 {
@@ -8,6 +9,7 @@ namespace RPG.Abilities
     public class Ability : ActionItem
     {
         [SerializeField] private TargetingStrategy targetingStrategy;
+        [SerializeField] private FilterStrategy[] filterStrategies;
 
         public override void Use(GameObject user)
         {
@@ -16,6 +18,12 @@ namespace RPG.Abilities
 
         private void TargetAcquired(IEnumerable<GameObject> targets)
         {
+            foreach (var filterStrategy in filterStrategies)
+            {
+                targets = filterStrategy.Filter(targets);
+            }
+            
+            
             foreach (var target in targets)
             {
                 Debug.Log(target);
