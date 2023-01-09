b4e9300587edebcd2c673121057c10cc2e1348c6
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2023-01-08 23:59:44 -0500

    Add mana regeneration

--- Assets/Scripts/Attributes/Mana.cs
+++ Assets/Scripts/Attributes/Mana.cs
@@ -1,19 +1,31 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace RPG.Attributes
 {
     public class Mana : MonoBehaviour
     {
         [SerializeField] private float maxMana = 15;
+        [SerializeField] private float manaRegenRate = 0;
 
         private float mana;
 
         private void Awake()
         {
             mana = maxMana;
+        }
+
+        private void Update()
+        {
+            if (mana < maxMana)
+            {
+                mana += manaRegenRate * Time.deltaTime;
+                if (mana > maxMana)
+                {
+                    mana = maxMana;
+                }
+            }
         }
 
         public float GetMana()
