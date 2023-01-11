447ad1590d1bf518abbf7e32eb54f5eef5adcf0b
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2023-01-10 21:17:36 -0500

    Add expected behavior to Traits UI
    Namely in that when you press a button to add or subtract to
    a given trait, it reflects in the Available Points how many are left and
    how many are put in each trait. Then when you press the Assign button
    the behavior updates as expected.

--- Assets/Scripts/Stats/TraitStore.cs
+++ Assets/Scripts/Stats/TraitStore.cs
@@ -6,31 +6,52 @@ namespace RPG.Stats
     public class TraitStore : MonoBehaviour
     {
         private Dictionary<Trait, int> assignedPoints = new Dictionary<Trait, int>();
+        private Dictionary<Trait, int> stagedPoints = new Dictionary<Trait, int>();
 
         private int unassignedPoints = 10;
+
+        public int GetProposedPoints(Trait trait)
+        {
+            return GetPoints(trait) + GetStagedPoints(trait);
+        }
 
         public int GetPoints(Trait trait)
         {
             return assignedPoints.ContainsKey(trait) ? assignedPoints[trait] : 0;
         }
 
+        public int GetStagedPoints(Trait trait)
+        {
+            return stagedPoints.ContainsKey(trait) ? stagedPoints[trait] : 0;
+        }
+
         public void AssignPoints(Trait trait, int points)
         {
             if (!CanAssignPoints(trait, points)) return;
-            
-            assignedPoints[trait] = GetPoints(trait) + points;
+
+            stagedPoints[trait] = GetStagedPoints(trait) + points;
             unassignedPoints -= points;
         }
 
         public bool CanAssignPoints(Trait trait, int points)
         {
-            if (GetPoints(trait) + points < 0) return false;
+            if (GetStagedPoints(trait) + points < 0) return false;
             return unassignedPoints >= points;
         }
 
         public int GetUnassignedPoints()
         {
             return unassignedPoints;
+        }
+
+        public void Commit()
+        {
+            foreach (var trait in stagedPoints.Keys)
+            {
+                assignedPoints[trait] = GetProposedPoints(trait);
+            }
+            
+            stagedPoints.Clear();
         }
     }
 }
